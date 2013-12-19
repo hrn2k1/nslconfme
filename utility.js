d17d2cfb923753835f24f05514ad28db4d9a495a
hrn2k1 <Harun.Rashid@dev.dctieto.fi>
2013-12-19 19:07:42 +0600

    utility.js added

---
+++ utility.js
@@ -0,0 +1,24 @@
+ 
+function Nullify(objval)
+{
+    return !objval?null:objval;
+}
+ function generateUid(separator) {
+    /// <summary>
+    ///    Creates a unique id for identification purposes.
+    /// </summary>
+    /// <param name="separator" type="String" optional="true">
+    /// The optional separator for grouping the generated segmants: default "-".    
+    /// </param>
+
+    var delim = separator || "-";
+
+    function S4() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+
+    return (S4() + S4() + delim + S4() + delim + S4() + delim + S4() + delim + S4() + S4() + S4());
+};
+
+exports.Nullify=Nullify;
+exports.generateUid=generateUid;
